68efe4f9cd084dfcdef89c23572a75b62de7b574
Aitor Quilez <aitorquilez@gmail.com>
2026-02-17 19:58:22 +0100

    Creación clases ViewModel

---
+++ app/src/main/java/com/example/qha_pmdm_2t/presentation/viewmodel/ProductosViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.qha_pmdm_2t.presentation.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.api_pmdm_t02.presentation.viewmodel.UiState
+import com.example.qha_pmdm_2t.data.model.ProductoDto
+import com.example.qha_pmdm_2t.data.remote.ProductosNetWork
+import com.example.qha_pmdm_2t.data.repository.ProductosRepository
+import kotlinx.coroutines.launch
+
+class ProductosViewModel : ViewModel() {
+
+    private val repository = ProductosRepository(ProductosNetWork.api)
+
+    var state by mutableStateOf<UiState<List<ProductoDto>>>(UiState.Loading)
+        private set
+
+    init {
+        loadProducts()
+    }
+
+    fun loadProducts(page: Int = 1) {
+        viewModelScope.launch {
+            state = UiState.Loading
+            state = try {
+                UiState.Success(repository.getProducts(page))
+            } catch (e: Exception) {
+                UiState.Error(e.message ?: "Error cargando productos")
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/qha_pmdm_2t/presentation/viewmodel/UiState.kt
@@ -0,0 +1,8 @@
+package com.example.qha_pmdm_2t.presentation.viewmodel
+
+sealed interface UiState<out T> {
+    data object Loading : UiState<Nothing>
+    data class Success<T>(val data: T) : UiState<T>
+    data class Error(val msg: String) : UiState<Nothing>
+
+}
