e408693fe98ff9342506bc9971a94ff9cf021fab
Aitor Quilez <aitorquilez@gmail.com>
2026-02-17 19:31:51 +0100

    Primer commit, creacion proyecto

---
+++ app/src/main/java/com/example/qha_pmdm_2t/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.qha_pmdm_2t
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.qha_pmdm_2t.ui.theme.QHA_PMDM_2TTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QHA_PMDM_2TTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QHA_PMDM_2TTheme {
+        Greeting("Android")
+    }
+}
